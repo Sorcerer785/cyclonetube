ed04a36e56d5f3e6b22f24d17376cc78e7f19293
Sorcerer785 <saurabhnarota1404@gmail.com>
2025-09-11 02:41:49 +0530

    routes added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, registerUser, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { loginUser , logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -25,4 +25,12 @@ userRouter.route("/login").post(loginUser)
 userRouter.route("/logout").post(verifyJWT, logoutUser)
 userRouter.route("/refresh-token").post(refreshAccessToken)
 
+userRouter.route("/change-password").post(verifyJWT,changeCurrentPassword)
+userRouter.route("/current-user").get(verifyJWT,getCurrentUser)
+userRouter.route("/update-account").patch(verifyJWT) // dont use post here as we dont want to change everything
+userRouter.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+userRouter.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+//while making req from params /c/: name
+userRouter.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+userRouter.route("/history").get(verifyJWT, getWatchHistory)
 export default userRouter
